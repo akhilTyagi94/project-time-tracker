c9af32acffde6d10b80b8d00ecf585ba033757e0
akhilTyagi94 <tyagi.akhil007@gmail.com>
2026-04-02 19:01:50 +0530

    resolved deployment issue for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient, Prisma } from '../generated/client';
 export { Prisma };
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
